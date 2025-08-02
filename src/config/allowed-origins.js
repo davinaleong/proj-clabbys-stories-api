e5af99e01c3177811e3f3d7953d14a2bf80d9a5d
Davina Leong <davina.leong.sy@gmail.com>
2025-08-02 21:33:42 +0800

    Fixed cors

--- src/config/allowed-origins.js
+++ src/config/allowed-origins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
   "http://localhost:3000", // local editor
-  "https://proj-clabbys-stories-editor-9r9j.vercel.app/", // production editor
+  "https://proj-clabbys-stories-editor-9r9j.vercel.app", // production editor
 ]
