92aef295b0e5617fa48fea058a1c5afa656656e4
Davina Leong <davina.leong.sy@gmail.com>
2025-08-02 17:27:58 +0800

    Added production editor to cors

--- src/config/allowed-origins.js
+++ src/config/allowed-origins.js
@@ -1,5 +1,4 @@
 export const allowedOrigins = [
-  "http://localhost:3000", // local dev
-  "https://your-vercel-app.vercel.app", // vercel preview
-  "https://your-custom-domain.com", // production domain
+  "http://localhost:3000", // local editor
+  "https://proj-clabbys-stories-editor-9r9j.vercel.app/", // production editor
 ]
